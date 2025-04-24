4278953d7320171f135329599a335cff9c6a6574
lapreindl <lapreindl@gmail.com>
2025-04-24 15:02:01 +0200

    added main

--- src/isogram.c
+++ src/isogram.c
@@ -3,6 +3,22 @@
 #include "isogram.h"
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+    // check of there is only one parameter
+    if (argc != 2)
+    {
+        // output a message otherwise (usage: isogram <phrase>)
+        printf("invalid input parameter! usage: isogram <phrase>\n");
+        return EXIT_FAILURE;
+    }
+    // call is_isogramm and pass the argument
+    // if result is true: print yes
+    // if result is false: print no
+    printf("%s\n", (is_isogram(argv[1]) == 0) ? "yes" : "no");
+}
 
 
 bool is_isogram(const char phrase[]){
